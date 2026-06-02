6fe57c37d6bedfc4a49d7f7d375c27af589a61c0
unknown <gervasiob@hotmail.com>
2026-06-01 22:54:34 -0300

    add knobs

--- input_controller.py
+++ input_controller.py
@@ -3,9 +3,10 @@ import pygame
 import time
 
 try:
-    import RPi.GPIO as GPIO
+    from gpiozero import RotaryEncoder, Button
 except Exception:
-    GPIO = None
+    RotaryEncoder = None
+    Button = None
 
 
 class KeyboardController:
@@ -56,106 +57,29 @@ class PhotoFrameController:
         invert_rotation=False,
     ):
         self._keyboard = KeyboardController()
+        self._encoder = None
 
-        self._pending_previous = 0
-        self._pending_next = 0
-        self._pending_click = 0
-
-        self._gpio_enabled = False
-        self._invert_rotation = invert_rotation
-        self._clk_pin = clk_pin
-        self._dt_pin = dt_pin
-        self._sw_pin = sw_pin
-
-        self._last_clk_state = 1
-        self._last_sw_state = 1
-        self._last_rotate_time = 0.0
-        self._rotate_debounce_sec = 0.001
-        self._last_click_time = 0.0
-        self._click_debounce_sec = 0.15
-
-        if enable_gpio:
-            self._setup_gpio()
-
-    def _setup_gpio(self):
-        if GPIO is None:
-            return
-
-        GPIO.setmode(GPIO.BCM)
-
-        GPIO.setup(
-            self._clk_pin,
-            GPIO.IN,
-            pull_up_down=GPIO.PUD_UP
-        )
-
-        GPIO.setup(
-            self._dt_pin,
-            GPIO.IN,
-            pull_up_down=GPIO.PUD_UP
-        )
-
-        GPIO.setup(
-            self._sw_pin,
-            GPIO.IN,
-            pull_up_down=GPIO.PUD_UP
-        )
-
-        self._last_clk_state = GPIO.input(self._clk_pin)
-        self._last_sw_state = GPIO.input(self._sw_pin)
-
-        self._gpio_enabled = True
+        if enable_gpio and RotaryEncoder is not None and Button is not None:
+            self._encoder = EncoderController(
+                clk_pin=clk_pin,
+                dt_pin=dt_pin,
+                sw_pin=sw_pin,
+                invert_rotation=invert_rotation,
+            )
 
     def close(self):
-        if GPIO is None or not self._gpio_enabled:
+        if self._encoder is None:
             return
 
         try:
-            GPIO.cleanup(
-                (self._clk_pin, self._dt_pin, self._sw_pin)
-            )
+            self._encoder.close()
         except Exception:
             pass
 
-        self._gpio_enabled = False
-
     def update(self):
-        if GPIO is None or not self._gpio_enabled:
+        if self._encoder is None:
             return
-
-        clk_state = GPIO.input(self._clk_pin)
-
-        if clk_state != self._last_clk_state:
-            self._last_clk_state = clk_state
-
-            if clk_state == 0:
-                now = time.monotonic()
-
-                if now - self._last_rotate_time >= self._rotate_debounce_sec:
-                    self._last_rotate_time = now
-
-                    dt_state = GPIO.input(self._dt_pin)
-                    is_next = (dt_state != clk_state)
-
-                    if self._invert_rotation:
-                        is_next = not is_next
-
-                    if is_next:
-                        self._pending_next += 1
-                    else:
-                        self._pending_previous += 1
-
-        sw_state = GPIO.input(self._sw_pin)
-
-        if sw_state != self._last_sw_state:
-            self._last_sw_state = sw_state
-
-            if sw_state == 0:
-                now = time.monotonic()
-
-                if now - self._last_click_time >= self._click_debounce_sec:
-                    self._last_click_time = now
-                    self._pending_click += 1
+        self._encoder.poll()
 
     def handle_event(self, event):
         self._keyboard.handle_event(event)
@@ -163,29 +87,105 @@ class PhotoFrameController:
     def previous(self):
         if self._keyboard.previous():
             return True
-
-        if self._pending_previous > 0:
-            self._pending_previous -= 1
-            return True
-
-        return False
+        return (
+            self._encoder is not None
+            and self._encoder.previous()
+        )
 
     def next(self):
         if self._keyboard.next():
             return True
-
-        if self._pending_next > 0:
-            self._pending_next -= 1
-            return True
-
-        return False
+        return (
+            self._encoder is not None
+            and self._encoder.next()
+        )
 
     def click(self):
         if self._keyboard.click():
             return True
+        return (
+            self._encoder is not None
+            and self._encoder.click()
+        )
 
-        if self._pending_click > 0:
-            self._pending_click -= 1
-            return True
 
-        return False
+class EncoderController:
+
+    def __init__(
+        self,
+        clk_pin=17,
+        dt_pin=18,
+        sw_pin=27,
+        invert_rotation=False,
+    ):
+        self.encoder = RotaryEncoder(
+            a=clk_pin,
+            b=dt_pin,
+            wrap=True
+        )
+
+        self.button = Button(sw_pin)
+
+        self._invert_rotation = invert_rotation
+        self._previous = False
+        self._next = False
+        self._click = False
+
+        self._last_steps = self.encoder.steps
+        self._last_button_pressed = self.button.is_pressed
+        self._last_click_time = 0.0
+        self._click_debounce_sec = 0.20
+
+    def poll(self):
+        current_steps = self.encoder.steps
+        delta = current_steps - self._last_steps
+
+        if delta != 0:
+            is_next = delta > 0
+
+            if self._invert_rotation:
+                is_next = not is_next
+
+            if is_next:
+                self._next = True
+            else:
+                self._previous = True
+
+        self._last_steps = current_steps
+
+        pressed = self.button.is_pressed
+
+        if pressed and not self._last_button_pressed:
+            now = time.monotonic()
+
+            if now - self._last_click_time >= self._click_debounce_sec:
+                self._last_click_time = now
+                self._click = True
+
+        self._last_button_pressed = pressed
+
+    def close(self):
+        try:
+            self.encoder.close()
+        except Exception:
+            pass
+
+        try:
+            self.button.close()
+        except Exception:
+            pass
+
+    def previous(self):
+        value = self._previous
+        self._previous = False
+        return value
+
+    def next(self):
+        value = self._next
+        self._next = False
+        return value
+
+    def click(self):
+        value = self._click
+        self._click = False
+        return value
