983e83e39567ea8ed7f95ea5d93ab1c410eda36f
unknown <gervasiob@hotmail.com>
2026-08-08 23:26:30 -0300

    update

--- controllers.py
+++ controllers.py
@@ -184,8 +184,8 @@ class MenuKnobController:
 
     def __init__(
         self,
-        menu_clk_pin=26,
-        menu_dt_pin=16,
+        menu_clk_pin=22,
+        menu_dt_pin=23,
         menu_sw_pin=24,
         invert_wheel_rotation=False,
     ):
